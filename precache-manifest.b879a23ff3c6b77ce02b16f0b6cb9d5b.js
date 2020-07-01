79c529edf2e4ed4efe3ecfc1494fcdd5b2c75f63
pavankumarvanam <v.pavan@ailabs.in>
2020-07-01 18:20:10 +0530

    Updates

---
+++ precache-manifest.b879a23ff3c6b77ce02b16f0b6cb9d5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a456f00172c9023ddb8c34132de0272f",
+    "url": "/react-crud-hooks/index.html"
+  },
+  {
+    "revision": "afe52c0e5415b54ea95f",
+    "url": "/react-crud-hooks/static/css/main.1ab163b6.chunk.css"
+  },
+  {
+    "revision": "1826da2b55172e383224",
+    "url": "/react-crud-hooks/static/js/2.21417d08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-crud-hooks/static/js/2.21417d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afe52c0e5415b54ea95f",
+    "url": "/react-crud-hooks/static/js/main.3b1d0519.chunk.js"
+  },
+  {
+    "revision": "3bb8f9b2e244d24c3b26",
+    "url": "/react-crud-hooks/static/js/runtime-main.d4068636.js"
+  }
+]);
